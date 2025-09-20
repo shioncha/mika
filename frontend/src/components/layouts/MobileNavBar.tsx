3125f64eabbd4fd998da7e46dbe1e5b5a4c56d30
shioncha <57836246+shioncha@users.noreply.github.com>
2025-09-20 06:46:46 +0000

    [frontend] Update: refactor MobileNavBar

--- frontend/src/components/layouts/MobileNavBar.tsx
+++ frontend/src/components/layouts/MobileNavBar.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import type { IconType } from "react-icons";
-import { CiHome, CiSettings } from "react-icons/ci";
+import { CiCirclePlus, CiHome, CiLock, CiSettings } from "react-icons/ci";
 import { IoIosAdd } from "react-icons/io";
 import { Link, useLocation } from "react-router";
 
@@ -13,7 +13,7 @@ interface NavItem {
   link: string;
 }
 
-const navItems: NavItem[] = [
+const signedNavItems: NavItem[] = [
   {
     label: "Timeline",
     icon: CiHome,
@@ -31,61 +31,51 @@ const navItems: NavItem[] = [
   },
 ];
 
+const guestNavItems: NavItem[] = [
+  {
+    label: "Top",
+    icon: CiHome,
+    link: "/",
+  },
+  {
+    label: "Sign In",
+    icon: CiLock,
+    link: "/signin",
+  },
+  {
+    label: "Sign Up",
+    icon: CiCirclePlus,
+    link: "/signup",
+  },
+];
+
 function MobileNavBar() {
   const { user } = useContext(UserContext);
   const [active, setActive] = useState<string>("");
   const location = useLocation();
 
+  const navItems = user ? signedNavItems : guestNavItems;
+
   useEffect(() => {
-    const path = location.pathname;
-    switch (path) {
-      case "/":
-        setActive("Timeline");
-        break;
-      case "/new":
-        setActive("New");
-        break;
-      case "/settings":
-        setActive("Settings");
-        break;
-      default:
-        break;
-    }
+    setActive(location.pathname);
   }, [location]);
 
   return (
     <div className={style.container}>
       <ul className={style.nav}>
-        {user ? (
-          <>
-            {navItems.map((item) => (
-              <li key={item.label}>
-                <Link
-                  to={item.link}
-                  className={`${style.link} ${
-                    active == item.label ? style.active : undefined
-                  }`}
-                >
-                  <item.icon size={24} />
-                  <span className={style.label}>{item.label}</span>
-                </Link>
-              </li>
-            ))}
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/signin" className={style.link}>
-                Sign in
-              </Link>
-            </li>
-            <li>
-              <Link to="/signup" className={style.link}>
-                Sign up
-              </Link>
-            </li>
-          </>
-        )}
+        {navItems.map((item) => (
+          <li key={item.label}>
+            <Link
+              to={item.link}
+              className={`${style.link} ${
+                active == item.link ? style.active : undefined
+              }`}
+            >
+              <item.icon size={24} />
+              <span className={style.label}>{item.label}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
